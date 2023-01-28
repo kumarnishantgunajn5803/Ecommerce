6eb67d142c6af8cda5a1afa6015db0f5a04407fd
Nishant Kumar <nkgunjan26102003@gmail.com>
2023-01-28 20:50:06 +0530

    created backend template

---
+++ EcomBakend/src/main/java/com/application/EcomBakendApplication.java
@@ -0,0 +1,13 @@
+package com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomBakendApplication.class, args);
+	}
+
+}
