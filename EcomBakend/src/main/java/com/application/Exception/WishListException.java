0548a8a7eca10a220fa5516f3bab151d6276c8b8
Nishant Kumar <nkgunjan26102003@gmail.com>
2023-02-12 10:11:03 +0530

    created wishListException

---
+++ EcomBakend/src/main/java/com/application/Exception/WishListException.java
@@ -0,0 +1,15 @@
+package com.application.Exception;
+
+public class WishListException extends RuntimeException{
+
+	public WishListException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public WishListException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+  
+}
