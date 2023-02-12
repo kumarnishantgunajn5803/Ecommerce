9452c0487f4f4146b6232568e136d1fcdcfe4fac
Nishant Kumar <nkgunjan26102003@gmail.com>
2023-02-12 10:27:18 +0530

    created WishList Service Interface

---
+++ EcomBakend/src/main/java/com/application/Service/WishListService.java
@@ -0,0 +1,17 @@
+package com.application.Service;
+
+import java.util.List;
+
+import com.application.Entities.Product;
+import com.application.Entities.WishList;
+import com.application.Exception.ProductException;
+import com.application.Exception.WishListException;
+
+public interface WishListService {
+	
+  public void addProductToWishList(Product product) throws WishListException , ProductException;
+  
+  public List<WishList> showWishList() throws WishListException;
+  
+  public void deleteProductFromWishList(Integer productId)throws WishListException, ProductException;
+}
