910033e5a7589e07cc7972e370d6362796f129ec
Nishant Kumar <nkgunjan26102003@gmail.com>
2023-02-16 13:26:45 +0530

    created OrderDetail Entity class

---
+++ EcomBakend/src/main/java/com/application/Entities/OrderDetail.java
@@ -0,0 +1,42 @@
+package com.application.Entities;
+
+import java.util.Date;
+
+ 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+ 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+
+public class OrderDetail {
+	
+	@Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Integer orderId;
+	
+	 
+   private String userFullName;
+   private Date createdDate;
+   private String address;
+   private String contactNumber;
+   private String alternateContactNumber;
+   private String orderStatus;
+   private double orderAmount;
+   
+   
+   
+}
